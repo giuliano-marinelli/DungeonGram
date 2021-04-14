8e2383f466f9929f9d7e2b63ce4b188a634e6e58
Giuliano Marinelli <giuliano.marinelli@fi.uncoma.edu.ar>
2021-04-13 21:25:12 -0300

    added improved about page and patreon buttons.

--- client/app/about/about.component.ts
+++ client/app/about/about.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, ChangeDetectorRef, Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Router, NavigationEnd } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +12,13 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
-  constructor() { }
+  constructor(
+    public router: Router,
+    public auth: AuthService,
+    private modalService: NgbModal
+  ) { }
 
+  openRegister() {
+    this.modalService.open(RegisterComponent);
+  }
 }
